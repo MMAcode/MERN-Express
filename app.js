7cc20ea495930776393d9e5ff553b013ae3ea723
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-04-13 10:02:40 +0100

    s

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const logger = require("morgan");
 
 const port = process.env.PORT || 3001;
 
-app.use(logger('dev'));
-app.use(cors()); //to enable Cross-Origin Resource Sharing
+// app.use(logger('dev'));
+// app.use(cors()); //to enable Cross-Origin Resource Sharing
+
 
 // NOT working - from React
 // app.use(express.static("build-copy"));
@@ -20,13 +21,21 @@ app.use(cors()); //to enable Cross-Origin Resource Sharing
 //NOT working ONLY online
 // app.use(express.static("test"));    //works locally, NOT online
 // app.get('/app', function (req, res, next) {  res.sendFile(path.join(__dirname, 'test', 'index.html')); });
-//app.get('/app', function (req, res, next) { res.sendFile(__dirname + '/test/index.html'); });
+// app.get('/app', function (req, res, next) { res.sendFile(__dirname + '/test/index.html'); });
 
-app.get('a', (req, res) => res.sendFile(__dirname + '/test/index.html'));
+// app.get('/a', (req, res) => res.sendFile(__dirname + '/test/index.html'));
+app.get('/a', (req, res) => {
+  console.log("ahoj from /a route");
+  res.sendFile(__dirname + '/test/index.html');
+});  
+// app.get('a', (req, res) => res.sendFile(__dirname + '/test/index.html')); - not working at all
 
 
 //OK
-app.get('/', (req, res) => res.sendFile(__dirname + '/html/index.html'));   //works
+app.get('/', (req, res) => {
+  console.log("ahoj from home route");
+  res.sendFile(__dirname + '/html/index.html');
+});   //works
 
 
 var path = require('path');
