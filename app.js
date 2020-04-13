51bb812de1702148aa4011b7d490ebbf215b0a4c
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-04-13 09:30:44 +0100

    path

--- app.js
+++ app.js
@@ -10,25 +10,28 @@ const logger = require("morgan");
 const port = process.env.PORT || 3001;
 
 app.use(logger('dev'));
-
 app.use(cors()); //to enable Cross-Origin Resource Sharing
 
-
-
-// const playersRouter = require("./routes/players");
-// app.use("/players", playersRouter);
-
-
-
-
-// module.exports = app;
-
-// app.get('/', (req, res) => res.sendFile(__dirname+'/html/serverHtmlTest.html'));
-
+ // NOT working - from React
 // app.use(express.static("build-copy"));
-app.use(express.static("test"));
+// app.get('/', (req, res) => res.sendFile(__dirname + '/build-copy/index.html')); 
 
-// app.get('/', (req, res) => res.sendFile(__dirname + '/html/index.html'));
+
+// app.use(express.static("test"));    //works locally, NOT online
+// app.get('/', (req, res) => res.sendFile(__dirname + '/html/index.html'));   //works
+// app.get('/', (req, res) => res.sendFile(__dirname + '/test/index.html'));   //works
+
+var path = require('path');
+
+/* GET home page. */
+app.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+/* GET React App */
+app.get('/app', function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'test', 'index.html'));
+});
+
 
 
 app.listen(port, () => console.log(`Miro's server-express listening at http://localhost:${port}.`))
