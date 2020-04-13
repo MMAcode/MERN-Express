acc8a51ab0a9d53e558f38abaeb31673fc4521db
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-04-13 09:35:05 +0100

    j

--- app.js
+++ app.js
@@ -12,13 +12,17 @@ const port = process.env.PORT || 3001;
 app.use(logger('dev'));
 app.use(cors()); //to enable Cross-Origin Resource Sharing
 
- // NOT working - from React
+// NOT working - from React
 // app.use(express.static("build-copy"));
 // app.get('/', (req, res) => res.sendFile(__dirname + '/build-copy/index.html')); 
 
 
+//NOT working online
 // app.use(express.static("test"));    //works locally, NOT online
-// app.get('/', (req, res) => res.sendFile(__dirname + '/html/index.html'));   //works
+
+
+//OK
+app.get('/', (req, res) => res.sendFile(__dirname + '/html/index.html'));   //works
 // app.get('/', (req, res) => res.sendFile(__dirname + '/test/index.html'));   //works
 
 var path = require('path');
